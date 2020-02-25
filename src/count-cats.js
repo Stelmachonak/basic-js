01973216557dcded79aa95668ad0c540925ff4e3
Stelmachonak <362060@mail.ru>
2020-02-25 14:27:25 +0300

    feat:add solution for task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let n = 0; n < matrix[i].length; n++) {
+      if (matrix[i][n] === "^^") {
+        count++;
+      }
+    }
+  }
+  return count;
 };
