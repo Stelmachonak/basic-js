f694bdb70dd417a77a39205d371328d18b2d2c0c
Stelmachonak <362060@mail.ru>
2020-02-25 16:51:17 +0300

    feat:add solution for task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,27 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+// year = ln (MODERN_ACTIVITY / sampleActivity)
+//         _____________________________________
+//         0.693 / HALF_LIFE_PERIOD
+
+module.exports = function dateSample(sampleActivity) {
+  let year, numerator, denominator;
+  let str = sampleActivity;
+  if (typeof str !== "string") {
+    return false;
+  }
+  let N = parseFloat(sampleActivity);
+  if (isNaN(N)) {
+    return false;
+  }
+  if (N <= 0 || N > 15) {
+    return false;
+  }
+
+  numerator = Math.log(MODERN_ACTIVITY / N);
+  denominator = 0.693 / HALF_LIFE_PERIOD;
+  year = Math.ceil(numerator / denominator);
+  return year;
 };
+
