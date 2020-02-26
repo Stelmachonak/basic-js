facc9ecdd447cd7e37db4c4e455883657f4a3e73
Stelmachonak <362060@mail.ru>
2020-02-27 00:02:52 +0300

    feat:add solution for task what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,28 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let temp, str, secretName;
+  let tempArr = [],
+    tempArr2 = [];
+  if (Array.isArray(members) === false) {
+    return false;
+  }
+  for (let j = 0; j < members.length; j++) {
+    if (typeof members[j] !== "string") {
+      members[j] = "";
+    } else {
+      temp = members[j].toUpperCase();
+      tempArr2.push(temp);
+    }
+  }
+
+  let sortMembers = tempArr2
+    .join(",")
+    .replace(/\s/g, "")
+    .split(",")
+    .sort();
+  for (let i = 0; i < sortMembers.length; i++) {
+    temp = sortMembers[i].slice(0, 1);
+    tempArr.push(temp);
+    secretName = tempArr.join("").replace(/\d|\[|\]/g, "");
+  }
+  return secretName;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,33 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let season = ["winter", "spring", "summer", "autumn"];
+  if (date === undefined) {
+    return "Unable to determine the time of year!";
+  }
+
+  if (Object.prototype.toString.call(date) !== "[object Date]") {
+    throw new Error();
+  }
+
+  switch (date.getMonth()) {
+    case 0:
+    case 1:
+    case 11:
+      return season[0];
+      break;
+    case 2:
+    case 3:
+    case 4:
+      return season[1];
+      break;
+    case 5:
+    case 6:
+    case 7:
+      return season[2];
+      break;
+    case 8:
+    case 9:
+    case 10:
+      return season[3];
+      break;
+  }
 };
