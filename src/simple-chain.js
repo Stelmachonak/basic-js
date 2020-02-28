b64387318ecc66ab24eeb4e5415ef7062be500e7
Stelmachonak <362060@mail.ru>
2020-02-28 13:57:01 +0300

    feat: add solution for task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,41 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== "number") {
+      this.chain = [];
+      throw new Error();
+    }
+
+    position--;
+    if (position < 0 || position >= this.getLength()) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let myChain = "";
+    for (let i = 0; i < this.getLength(); i++) {
+      myChain += `( ${this.chain[i]} )`;
+      if (i < this.getLength() - 1) {
+        myChain += "~~";
+      }
+    }
+    this.chain = [];
+    return myChain;
   }
 };
 
